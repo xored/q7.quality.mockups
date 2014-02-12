11c6a0d7b830d2b001b1f97252f20cb5014b18f6
Andrey Sobolev <haiodo@xored.com>
2014-02-12 13:42:20 +0700

    * more complex gef without editpart mockup

--- plugins/com.xored.q7.quality.mockups.draw2d/src/com/xored/q7/quality/mockups/draw2d/GraphicalViewerPart.java
+++ plugins/com.xored.q7.quality.mockups.draw2d/src/com/xored/q7/quality/mockups/draw2d/GraphicalViewerPart.java
@@ -3,9 +3,11 @@ package com.xored.q7.quality.mockups.draw2d;
 import org.eclipse.draw2d.CheckBox;
 import org.eclipse.gef.GraphicalViewer;
 import org.eclipse.gef.ui.parts.GraphicalViewerImpl;
+import org.eclipse.gef.ui.parts.ScrollingGraphicalViewer;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 
@@ -17,6 +19,8 @@ public class GraphicalViewerPart extends BaseMockupPart {
 	public Control construct(Composite parent) {
 		Composite content = new Composite(parent, SWT.NONE);
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(content);
+		Canvas cv = new Canvas(content, SWT.BORDER);
+		GridDataFactory.fillDefaults().grab(true, false).hint(SWT.DEFAULT, 20).applyTo(cv);
 		GridLayoutFactory.fillDefaults().applyTo(content);
 		GraphicalViewer gv = new GraphicalViewerImpl() {
 			@Override
@@ -26,6 +30,15 @@ public class GraphicalViewerPart extends BaseMockupPart {
 		};
 		gv.createControl(content);
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(gv.getControl());
+
+		GraphicalViewer gv2 = new ScrollingGraphicalViewer() {
+			@Override
+			protected void createDefaultRoot() {
+				setRootFigure(new CheckBox("Foo2 bar2 baz2"));
+			}
+		};
+		gv2.createControl(content);
+		GridDataFactory.fillDefaults().grab(true, true).applyTo(gv2.getControl());
 
 		return content;
 	}
