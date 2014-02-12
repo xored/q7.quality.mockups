d23ed05a42c3e828297a3aafb3dca429c323f99b
Ivan Inozemtsev <ivan.inozemtsev@xored.com>
2014-02-12 18:50:49 +0700

    Created mockup for QS-3190

---
+++ plugins/com.xored.q7.quality.mockups.zest/src/com/xored/q7/quality/mockups/zest/ZestMockup.java
@@ -0,0 +1,70 @@
+package com.xored.q7.quality.mockups.zest;
+
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.zest.core.widgets.Graph;
+import org.eclipse.zest.core.widgets.GraphConnection;
+import org.eclipse.zest.core.widgets.GraphNode;
+import org.eclipse.zest.core.widgets.ZestStyles;
+import org.eclipse.zest.layouts.LayoutStyles;
+import org.eclipse.zest.layouts.algorithms.SpringLayoutAlgorithm;
+
+import com.xored.q7.quality.mockups.issues.BaseMockupPart;
+
+public class ZestMockup extends BaseMockupPart {
+
+	@Override
+	public String getLabel() {
+		return null;
+	}
+
+	@Override
+	public Control construct(Composite parent) {
+		Composite content = new Composite(parent, SWT.NONE);
+		GridDataFactory.fillDefaults().grab(true, true).applyTo(content);
+		GridLayoutFactory.fillDefaults().numColumns(1).applyTo(content);
+
+		Graph graph = new Graph(content, SWT.NONE);
+		GridDataFactory.fillDefaults().grab(true, true).applyTo(graph);
+		// now a few nodes
+		GraphNode node1 = new GraphNode(graph, SWT.NONE, "Jim");
+		GraphNode node2 = new GraphNode(graph, SWT.NONE, "Jack");
+		GraphNode node3 = new GraphNode(graph, SWT.NONE, "Joe");
+		GraphNode node4 = new GraphNode(graph, SWT.NONE, "Bill");
+		// Lets have a directed connection
+		new GraphConnection(graph, ZestStyles.CONNECTIONS_DIRECTED, node1,
+				node2);
+		// Lets have a dotted graph connection
+		new GraphConnection(graph, ZestStyles.CONNECTIONS_DOT, node2, node3);
+		// Standard connection
+		new GraphConnection(graph, SWT.NONE, node3, node1);
+		// Change line color and line width
+		GraphConnection graphConnection = new GraphConnection(graph, SWT.NONE,
+				node1, node4);
+		graphConnection.changeLineColor(parent.getDisplay().getSystemColor(SWT.COLOR_GREEN));
+		// Also set a text
+		graphConnection.setText("This is a text");
+		graphConnection.setHighlightColor(parent.getDisplay().getSystemColor(SWT.COLOR_RED));
+		graphConnection.setLineWidth(3);
+
+		graph.setLayoutAlgorithm(new SpringLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), true);
+		// Selection listener on graphConnect or GraphNode is not supported
+		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=236528
+		graph.addSelectionListener(new SelectionAdapter() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println(e);
+			}
+
+		});
+
+		return content;
+	}
+
+}
