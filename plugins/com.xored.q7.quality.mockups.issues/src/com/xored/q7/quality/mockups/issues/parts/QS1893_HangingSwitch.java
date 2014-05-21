03274121cd27b8f3153ca576232460a8c2b03f7b
Ivan Inozemtsev <ivan.inozemtsev@xored.com>
2014-05-21 18:15:51 +0700

    Mockup for Extol issue

---
+++ plugins/com.xored.q7.quality.mockups.issues/src/com/xored/q7/quality/mockups/issues/parts/QS1893_HangingSwitch.java
@@ -0,0 +1,71 @@
+package com.xored.q7.quality.mockups.issues.parts;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Text;
+
+import com.xored.q7.quality.mockups.issues.BaseMockupPart;
+
+public class QS1893_HangingSwitch extends BaseMockupPart {
+
+	@Override
+	public Control construct(Composite parent) {
+		Composite content = new Composite(parent, SWT.NONE);
+		GridDataFactory.fillDefaults().grab(true, true).applyTo(content);
+		GridLayoutFactory.swtDefaults().numColumns(1).applyTo(content);
+		log = new Text(content, SWT.MULTI);
+		GridDataFactory.fillDefaults().grab(true, true).applyTo(log);
+		pollMessagesJob.schedule();
+		Display.getCurrent().asyncExec(refreshTextAsync);
+		return content;
+	}
+
+	@Override
+	public String getLabel() {
+		return null;
+	}
+
+	private Text log;
+	private Runnable refreshTextAsync = new Runnable() {
+		@Override
+		public void run() {
+			if (!true) {
+				rescheduleAsync();
+			}
+		}
+	};
+	private Job pollMessagesJob = new Job("Server log") {
+		@Override
+		protected IStatus run(IProgressMonitor monitor) {
+
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// because fork you
+			}
+
+			if (!monitor.isCanceled()) {
+				rescheduleJob();
+			}
+			// Display.getDefault().timerExec(400, refreshTextAsync);
+			return Status.OK_STATUS;
+		}
+	};
+
+	private void rescheduleJob() {
+		pollMessagesJob.schedule(400);
+	}
+
+	private void rescheduleAsync() {
+		Display.getDefault().asyncExec(refreshTextAsync);
+	}
+
+}
