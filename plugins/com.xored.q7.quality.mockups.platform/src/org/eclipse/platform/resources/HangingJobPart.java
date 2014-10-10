ebca14d38ff8ab273095935ce12e28f608b29f89
Gulevich Vasili <vasili.gulevich@xored.com>
2014-10-10 20:31:04 +0700

    Hanging job mockup

--- plugins/com.xored.q7.quality.mockups.platform/src/org/eclipse/platform/resources/HangingJobPart.java
+++ plugins/com.xored.q7.quality.mockups.platform/src/org/eclipse/platform/resources/HangingJobPart.java
@@ -1,6 +1,10 @@
 package org.eclipse.platform.resources;
 
+import static com.xored.q7.quality.mockups.swt.internal.QualityPlatformPlugin.PLUGIN_ID;
+import static java.util.Arrays.asList;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -8,22 +12,26 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
-import org.eclipse.core.runtime.jobs.IJobChangeListener;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ComboViewer;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.ListViewer;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Spinner;
 
 import com.xored.q7.quality.mockups.issues.BaseMockupPart;
-import com.xored.q7.quality.mockups.swt.internal.QualityPlatformPlugin;
 
 /**
  * Emulates hanged job with specified parameters.
@@ -31,53 +39,77 @@ import com.xored.q7.quality.mockups.swt.internal.QualityPlatformPlugin;
  * */
 public class HangingJobPart extends BaseMockupPart {
 
-	private Label jobsStartedLabel;
-	private Combo hangTypeCombo;
-	private Combo hangThreadCombo;
-	private Spinner jobHangTimeoutSpinner;
-	private List<Job> startedJobs = new ArrayList<Job>();
+	interface Function<Argument, Return> {
+		Return apply(Argument input);
+	}
+
+	private static final IStatus[] STATUSES = new IStatus[] {
+			new Status(IStatus.OK, PLUGIN_ID, "Job OK"),
+			new Status(IStatus.INFO, PLUGIN_ID, "Job info"),
+			new Status(IStatus.WARNING, PLUGIN_ID, "Job warning"),
+			new Status(IStatus.ERROR, PLUGIN_ID, "Job error")
+	};
+
+	private int count = 0;
+
+	private final List<String> activeJobs = Collections.synchronizedList(new ArrayList<String>());
+
+	private ListViewer activeJobsViewer;
+
+	private static Object getSelected(final ComboViewer combo) {
+		return ((IStructuredSelection) combo.getSelection()).getFirstElement();
+	}
 
 	@Override
 	public Control construct(Composite parent) {
-		GridLayoutFactory.swtDefaults().applyTo(parent);
-		final Composite container = new Composite(parent, SWT.NONE);
-		GridLayoutFactory.swtDefaults().numColumns(2).applyTo(container);
+		GridLayoutFactory.swtDefaults().numColumns(2).applyTo(parent);
 
-		Label hangTypeLabel = new Label(container, SWT.NONE);
-		hangTypeLabel.setText("Hang Type:");
-		hangTypeCombo = new Combo(container, SWT.NONE);
-		hangTypeCombo.setItems(new String[] { "Infinite loop", "Thread.sleep()", "Object.wait()" });
-		hangTypeCombo.select(0);
+		Label label = new Label(parent, SWT.NONE);
+		label.setText("Hang type:");
+		final ComboViewer hangTypeCombo = createCombo(parent, asList(HangType.values()));
 
-		Label jobHangTimeoutLabel = new Label(container, SWT.NONE);
-		jobHangTimeoutLabel.setText("Hang Timeout (ms):");
-		jobHangTimeoutSpinner = new Spinner(container, SWT.BORDER);
-		jobHangTimeoutSpinner.setMinimum(0);
-		jobHangTimeoutSpinner.setMaximum(180000);
-		jobHangTimeoutSpinner.setSelection(10000);
+		label = new Label(parent, SWT.NONE);
+		label.setText("Thread type:");
+		final ComboViewer threadTypeCombo = createCombo(parent, asList(ThreadType.values()));
 
-		Label jobHangThreadLabel = new Label(container, SWT.NONE);
-		jobHangThreadLabel.setText("Hang Type:");
-		hangThreadCombo = new Combo(container, SWT.NONE);
-		hangThreadCombo.setItems(new String[] { "Display", "Schedule" });
-		hangThreadCombo.select(0);
+		label = new Label(parent, SWT.NONE);
+		label.setText("Return status:");
+		final ComboViewer statusCombo = createCombo(parent, asList(STATUSES));
+		statusCombo.setLabelProvider(new LabelProvider() {
 
-		Label jobsStartedTextLabel = new Label(container, SWT.NONE);
-		jobsStartedTextLabel.setText("Jobs started:");
-		jobsStartedLabel = new Label(container, SWT.NONE);
-		jobsStartedLabel.setText("0");
+			@Override
+			public String getText(Object element) {
+				if (element instanceof IStatus)
+					return ((IStatus) element).getMessage();
+				return super.getText(element);
+			}
+		});
 
-		Button startNewJobButton = new Button(container, SWT.NONE);
+		label = new Label(parent, SWT.NONE);
+		label.setText("Hang time (ms):");
+		final Spinner jobHangTimeoutSpinner = createSpinner(parent);
+
+		label = new Label(parent, SWT.NONE);
+		label.setText("Delay (ms):");
+		final Spinner delaySpinner = createSpinner(parent);
+
+		
+
+		Button startNewJobButton = new Button(parent, SWT.NONE);
 		startNewJobButton.setText("Start New Job!");
 		startNewJobButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				super.widgetSelected(e);
-				startNewJob();
+				HangType hang = (HangType) getSelected(hangTypeCombo);
+				ThreadType thread = (ThreadType) getSelected(threadTypeCombo);
+				IStatus status = (IStatus) getSelected(statusCombo);
+				startNewJob(hang, delaySpinner.getSelection(), jobHangTimeoutSpinner.getSelection(), thread, status);
 			}
+
 		});
 
-		Button terminateAllJobsButton = new Button(container, SWT.NONE);
+		Button terminateAllJobsButton = new Button(parent, SWT.NONE);
 		terminateAllJobsButton.setText("Terminate All");
 		terminateAllJobsButton.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -87,96 +119,73 @@ public class HangingJobPart extends BaseMockupPart {
 			}
 		});
 
-		return container;
+		activeJobsViewer = new ListViewer(parent);
+		activeJobsViewer.setContentProvider(new ArrayContentProvider());
+		activeJobsViewer.setLabelProvider(new LabelProvider());
+		activeJobsViewer.setInput(activeJobs);
+		GridDataFactory.fillDefaults().span(2, 1).grab(true, true).applyTo(activeJobsViewer.getControl());
+		return parent;
 	}
 
-	private void startNewJob() {
-		HangType hangType;
-		switch (hangTypeCombo.getSelectionIndex()) {
-		case 0:
-		default:
-			hangType = HangType.INFINITE_LOOP;
-			break;
-		case 1:
-			hangType = HangType.THREAD_SLEEP;
-			break;
-		case 2:
-			hangType = HangType.OBJECT_WAIT;
-			break;
+	private Spinner createSpinner(final Composite container) {
+		final Spinner spinner = new Spinner(container, SWT.BORDER);
+		spinner.setMinimum(0);
+		spinner.setMaximum(180000);
+		spinner.setSelection(1000);
+		GridDataFactory.fillDefaults().grab(true, false).applyTo(spinner);
+		return spinner;
+	}
+
+	private static ComboViewer createCombo(final Composite container, List<?> values) {
+		ComboViewer combo = new ComboViewer(container);
+		combo.setContentProvider(new ArrayContentProvider());
+		combo.setLabelProvider(new LabelProvider());
+		combo.setInput(values);
+		combo.setSelection(new StructuredSelection(values.get(0)));
+		GridDataFactory.fillDefaults().grab(true, false).applyTo(combo.getControl());
+		return combo;
+	}
+
+	private Runnable activeJobsRefreshRunnable = new Runnable() {
+		@Override
+		public void run() {
+			activeJobsViewer.refresh(true);
+			while (activeJobsViewer.getControl().getDisplay().readAndDispatch())
+				;
+			activeJobsViewer.getControl().redraw();
 		}
+	};
 
-		final JobHangBehaviour hangBehaviour = new JobHangBehaviour(jobHangTimeoutSpinner.getSelection(), hangType);
-
-		if (hangThreadCombo.getSelectionIndex() == 0) {
-			try {
-				hangBehaviour.hang(new NullProgressMonitor());
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+	private void startNewJob(final HangType hangType, int delay, final int timeout, ThreadType thread, final IStatus rv) {
+		final String name;
+		synchronized (this) {
+			name = "Wait type: " + hangType + ", threading: " + thread + ", return: " + rv.getMessage() + ", delay "
+					+ delay + ", time: " + timeout + ", instance:" + count++;
+		}
+		thread.start(delay, new Function<IProgressMonitor, IStatus>() {
+			@Override
+			public IStatus apply(IProgressMonitor input) {
+				try {
+					activeJobs.add(name);
+					refresh();
+					hangType.hang(input, timeout);
+				} catch (InterruptedException e) {
+					return new Status(IStatus.WARNING, PLUGIN_ID, "Interrupted");
+				} finally {
+					activeJobs.remove(name);
+					refresh();
+				}
+				return rv;
 			}
-		} else {
-			Job job = new Job("Another Hanging Job") {
-				@Override
-				protected IStatus run(IProgressMonitor monitor) {
-					try {
-						hangBehaviour.hang(monitor);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-						return new Status(Status.ERROR, QualityPlatformPlugin.PLUGIN_ID, "Failed to hang!", e);
-					}
-					return Status.OK_STATUS;
-				}
-			};
 
-			job.addJobChangeListener(new IJobChangeListener() {
-				@Override
-				public void done(IJobChangeEvent event) {
-					if (startedJobs.contains(event.getJob())) {
-						startedJobs.remove(event.getJob());
-					}
-					updateStartedJobs();
-				}
-
-				@Override
-				public void sleeping(IJobChangeEvent event) {
-				}
-
-				@Override
-				public void scheduled(IJobChangeEvent event) {
-				}
-
-				@Override
-				public void running(IJobChangeEvent event) {
-				}
-
-				@Override
-				public void awake(IJobChangeEvent event) {
-				}
-
-				@Override
-				public void aboutToRun(IJobChangeEvent event) {
-				}
-			});
-
-			startedJobs.add(job);
-			job.schedule();
-		}
-		updateStartedJobs();
+			private void refresh() {
+				Display.getDefault().syncExec(activeJobsRefreshRunnable);
+			}
+		});
 	}
 
 	private void terminateAll() {
-		for (Job job : startedJobs) {
-			job.cancel();
-		}
-	}
-
-	private void updateStartedJobs() {
-		jobsStartedLabel.getDisplay().syncExec(new Runnable() {
-			@Override
-			public void run() {
-				jobsStartedLabel.setText(String.valueOf(startedJobs.size()));
-			}
-
-		});
+		Job.getJobManager().cancel(ThreadType.jobGroup);
 	}
 
 	@Override
@@ -184,47 +193,90 @@ public class HangingJobPart extends BaseMockupPart {
 		return "Hanging Jobs";
 	}
 
-	private static class JobHangBehaviour {
-		private int timeoutMs;
-		private HangType hangType;
-
-		public JobHangBehaviour(int timeoutMs, HangType hangType) {
-			this.timeoutMs = timeoutMs;
-			this.hangType = hangType;
-		}
-
-		public void hang(IProgressMonitor monitor) throws InterruptedException {
-			switch (hangType) {
-			case INFINITE_LOOP:
-				hangLoop(monitor);
-				break;
-			case OBJECT_WAIT:
-				this.wait(timeoutMs);
-				break;
-			case THREAD_SLEEP:
-				Thread.sleep(timeoutMs);
-				break;
-			default:
-				break;
-
+	private enum ThreadType {
+		DIRECT {
+			@Override
+			void start(int delay, Function<IProgressMonitor, IStatus> method) {
+				toRunnable(method).run();
 			}
-		}
+		},
+		SYNC_DISPLAY {
+			@Override
+			void start(int delay, final Function<IProgressMonitor, IStatus> method) {
+				Display.getDefault().syncExec(toRunnable(method));
+			}
+		},
 
-		private void hangLoop(IProgressMonitor monitor) {
-			Date startTime = new Date();
+		ASYNC_DISPLAY {
+			@Override
+			void start(int delay, Function<IProgressMonitor, IStatus> method) {
+				Display.getDefault().asyncExec(toRunnable(method));
+			}
+		},
+		TIMER_EXEC {
+			@Override
+			void start(int delay, Function<IProgressMonitor, IStatus> method) {
+				Display.getDefault().timerExec(delay, toRunnable(method));
+			}
+		},
+		JOB {
+			@Override
+			void start(int delay, final Function<IProgressMonitor, IStatus> method) {
+				Job job = new Job(method.getClass().getName()) {
+					@Override
+					protected IStatus run(IProgressMonitor monitor) {
+						return method.apply(monitor);
+					}
+				};
+				job.schedule(delay);
+			}
+		};
+		abstract void start(int delay, Function<IProgressMonitor, IStatus> method);
 
-			while (!monitor.isCanceled()) {
-				Date currentTime = new Date();
-				if (currentTime.getTime() - startTime.getTime() >= timeoutMs) {
-					break;
+		private final static Object jobGroup = new Object();
+	}
+
+	private static Runnable toRunnable(final Function<IProgressMonitor, IStatus> method) {
+		return new Runnable() {
+
+			@Override
+			public void run() {
+				final IStatus status = method.apply(new NullProgressMonitor());
+				if (!status.isOK()) {
+					throw new RuntimeException(status.getMessage());
 				}
 			}
-		}
+		};
 	}
 
 	private enum HangType {
-		INFINITE_LOOP,
-		THREAD_SLEEP,
-		OBJECT_WAIT
+		LOOP {
+			@Override
+			void hang(IProgressMonitor monitor, int timeout) {
+				Date startTime = new Date();
+				while (!monitor.isCanceled()) {
+					Date currentTime = new Date();
+					if (currentTime.getTime() - startTime.getTime() >= timeout) {
+						break;
+					}
+				}
+			}
+		},
+		THREAD_SLEEP {
+			@Override
+			void hang(IProgressMonitor monitor, int timeout) throws InterruptedException {
+				Thread.sleep(timeout);
+			}
+		},
+		OBJECT_WAIT {
+			@Override
+			void hang(IProgressMonitor monitor, int timeout) throws InterruptedException {
+				Object object = new Object();
+				synchronized (object) {
+					object.wait(timeout);
+				}
+			}
+		};
+		abstract void hang(IProgressMonitor monitor, int timeout) throws InterruptedException;
 	}
 }
