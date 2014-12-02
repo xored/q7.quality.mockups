43df15a596d5f0923613497e885956b2ddc03b5b
Gulevich Vasili <vasili.gulevich@xored.com>
2014-12-02 19:22:25 +0700

    RCPTT-21 Dialong link mockup now reports event details.

--- plugins/com.xored.q7.quality.mockups.swt/src/org/eclipse/form/FormWithLinks.java
+++ plugins/com.xored.q7.quality.mockups.swt/src/org/eclipse/form/FormWithLinks.java
@@ -1,31 +1,22 @@
 package org.eclipse.form;
 
-import java.net.URL;
-
 import javax.swing.event.HyperlinkEvent;
 
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.browser.IWebBrowser;
-import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
 import org.eclipse.ui.forms.HyperlinkGroup;
 import org.eclipse.ui.forms.events.HyperlinkAdapter;
-import org.eclipse.ui.forms.events.IHyperlinkListener;
 import org.eclipse.ui.forms.widgets.Form;
 import org.eclipse.ui.forms.widgets.FormText;
 import org.eclipse.ui.forms.widgets.FormToolkit;
@@ -133,93 +124,32 @@ public class FormWithLinks extends BaseMockupPart {
 
 			}
 		});
-		
-		final FormText rtext = toolkit.createFormText(client, true);
-		
-	   String data = "<form><p>This is a link to "+"<a href='http://google.com'>web site</a> lol lol </p></form>";
-       rtext.setText(data, true, false);	    
-       rtext.addHyperlinkListener(new IHyperlinkListener(){
 
-		@Override
-		public void linkActivated(
-				org.eclipse.ui.forms.events.HyperlinkEvent arg0) {
-			
-			//System.out.println("Link activated!");
-		
-			
-			
-			IWorkbenchBrowserSupport support=PlatformUI.getWorkbench().getBrowserSupport();
-			
-			
-			try {
-			IWebBrowser browser=support.getExternalBrowser();
-			
-			
-			
-			if (browser != null) {
-			URL url=new URL(rtext.getSelectedLinkHref().toString());
-			support.createBrowser("myId").openURL(url);
-			//browser.openURL(url);
+		final FormText rtext2 = toolkit.createFormText(client, true);
+
+		String data2 = "<form><p>This link opens a " + "<a href='http://google.com'> dialog </a>lol</p></form>";
+		rtext2.setText(data2, true, false);
+		rtext2.addHyperlinkListener(new HyperlinkAdapter() {
+			@Override
+			public void linkActivated(org.eclipse.ui.forms.events.HyperlinkEvent arg0) {
+				Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+				MessageDialog dialog = new MessageDialog(shell, "My Title", null,
+						"My message", MessageDialog.ERROR, new String[] { "First",
+								"Second", "Third" }, 0);
+				dialog.open();
+				tt.setVisible(true);
+				if (arg0 == null) {
+					tt.setText("Empty link");
+				} else {
+					tt.setText(arg0.getLabel() + " : " + arg0.getHref() + ":" + arg0.getStateMask());
+				}
 			}
-			}catch(Exception ex){
-				ex.printStackTrace();
-			}
-		}
+		});
 
-		@Override
-		public void linkEntered(org.eclipse.ui.forms.events.HyperlinkEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void linkExited(org.eclipse.ui.forms.events.HyperlinkEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-    		   
-    		  
-    		   });
-		
-       final FormText rtext2 = toolkit.createFormText(client, true);
-		
-	   String data2 = "<form><p>This link opens a "+"<a href='http://google.com'> dialog </a>lol</p></form>";
-      rtext2.setText(data2, true, false);	    
-      rtext2.addHyperlinkListener(new IHyperlinkListener(){
-    	  public void linkActivated(org.eclipse.ui.forms.events.HyperlinkEvent arg0) {
-    		  Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-    		  MessageDialog dialog = new MessageDialog(shell, "My Title", null,
-    				    "My message", MessageDialog.ERROR, new String[] { "First",
-    				  "Second", "Third" }, 0);
-    				dialog.open();
-    		  
-    	  } 
-    	  
-    	  
-    	  @Override
-  		public void linkEntered(org.eclipse.ui.forms.events.HyperlinkEvent arg0) {
-  			// TODO Auto-generated method stub
-  			
-  		}
-
-  		@Override
-  		public void linkExited(org.eclipse.ui.forms.events.HyperlinkEvent arg0) {
-  			// TODO Auto-generated method stub
-  			
-  		}
-    	  
-      });
-      
-
-      
-
-      
 		return null;
 	}
 
-	@Override
 	public String getLabel() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
