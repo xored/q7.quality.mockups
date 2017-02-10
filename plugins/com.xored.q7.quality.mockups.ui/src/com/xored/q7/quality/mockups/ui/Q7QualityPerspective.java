5cc7c2bc7931146b29f8e66e6a3336195c0eff04
Andrey Sobolev <andrey.sobolev@xored.com>
2017-02-10 20:43:07 +0600

    * fix perspective

--- plugins/com.xored.q7.quality.mockups.ui/src/com/xored/q7/quality/mockups/ui/Q7QualityPerspective.java
+++ plugins/com.xored.q7.quality.mockups.ui/src/com/xored/q7/quality/mockups/ui/Q7QualityPerspective.java
@@ -7,8 +7,11 @@ public class Q7QualityPerspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout layout) {
 		layout.addStandaloneView("com.xored.q7.quality.mockups.category.view",
-				false, 0, 0, layout.getEditorArea());
-		layout.setEditorAreaVisible(false);
+				true, IPageLayout.TOP, IPageLayout.RATIO_MAX, layout.getEditorArea());
+		
+	
+		
+		layout.setEditorAreaVisible(true);
 	}
 
 }
