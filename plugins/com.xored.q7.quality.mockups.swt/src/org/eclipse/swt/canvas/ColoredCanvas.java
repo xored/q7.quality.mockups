c4b40c3287f14f5840836f0e7c8b69e30c98a6c7
Vasili Gulevich <vasili.gulevich@xored.com>
2024-03-31 03:20:24 +0400

    Skip broken Canvas foreground test eclipse/org.eclipse.rcptt/#61

--- plugins/com.xored.q7.quality.mockups.swt/src/org/eclipse/swt/canvas/ColoredCanvas.java
+++ plugins/com.xored.q7.quality.mockups.swt/src/org/eclipse/swt/canvas/ColoredCanvas.java
@@ -4,11 +4,9 @@ import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.Device;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Display;
 
 import com.xored.q7.quality.mockups.issues.BaseMockupPart;
 
@@ -16,23 +14,14 @@ public class ColoredCanvas extends BaseMockupPart {
 
 	@Override
 	public Control construct(Composite parent) {
-		// TODO Auto-generated method stub
-		
 		Canvas canvas = new Canvas(parent, SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL);
-		
 		GridLayoutFactory.swtDefaults().numColumns(1).applyTo(parent);
 		GridDataFactory.swtDefaults().align(SWT.FILL, SWT.FILL)
 		    .grab(true, true).applyTo(canvas);
-		
-		
-		Device d = Display.getCurrent ();
-		Color red = new Color(d, 255, 0, 0);
+		Color red = new Color(parent.getDisplay(), 255, 0, 0);
+		canvas.addDisposeListener(event -> red.dispose());
 		canvas.setBackground(red);
 		canvas.setForeground(red);
-		
-		
-		
-		
 		return null;
 	}
 
